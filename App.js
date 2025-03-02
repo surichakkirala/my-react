205df0dec44d9545d39eb99eadf9df6215fc467b
surendra.c <surendra.c@jahoo.io>
2025-03-02 15:01:46 +0530

    Component Init commit

--- App.js
+++ App.js
@@ -6,8 +6,27 @@ import ReactDOM from "react-dom/client";
 // JSX=> Babel transpiles it to React.createElement => Object => HTML Element(render)
 const jsxHeading = (
   <h1 className="head" tabIndex="5">
-    Namaste React
+    Namaste React using React Element
   </h1>
 );
+
+//React Functional Component - A JS function which returns a React Element
+//Component Composition - putting one component inside another component
+
+const Title = () => {
+  return (
+    <p className="title" tabIndex="1">
+      Title Component
+    </p>
+  );
+};
+
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    <h1 className="heading">Namaste React using Component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
