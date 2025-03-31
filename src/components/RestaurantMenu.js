42ec2f5d87f002584643c75a864841f16f405c35
surichakkirala <surichakkirala@gmail.com>
2025-03-31 17:50:12 +0530

    accordion show hide change

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -31,7 +31,7 @@ const RestaurantMenu = () => {
           key={category?.card?.card?.title}
           data={category?.card?.card}
           showAccordion={index === showIndex ? true : false}
-          setShowIndex={() => setShowIndex(index)}
+          setShowIndex={() => setShowIndex(index === showIndex ? null : index)}
         />
       ))}
     </div>
